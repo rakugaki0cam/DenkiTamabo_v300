9b659f0220aae0706ad0503bb1017bd5aeef165b
rakugaki0cam <isober21@gmail.com>
2024-04-19 23:01:22 +0900

    scale pin G13..DOUT G14..SCK

--- src/loadcell.cpp
+++ src/loadcell.cpp
@@ -3,31 +3,39 @@
 
 #include "loadcell.hpp"
 
-HX711 load1;
+HX711 scale;
 
 void loadcellInit(void){
     // 1. HX711 circuit wiring
-    const int LOADCELL_DOUT_PIN = 2;
-    const int LOADCELL_SCK_PIN = 3;
+    const int LOADCELL_DOUT_PIN = 13;
+    const int LOADCELL_SCK_PIN = 14;
 
     // 2. Adjustment settings
-    const long LOADCELL_OFFSET = 50682624;
-    const long LOADCELL_DIVIDER = 5895655;
+    //const long LOADCELL_OFFSET = 50682624;
+    const long LOADCELL_DIVIDER = 3192.f;
 
     // 3. Initialize library
-    load1.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-    load1.set_scale(LOADCELL_DIVIDER);
-    load1.set_offset(LOADCELL_OFFSET);
+    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    scale.set_scale(LOADCELL_DIVIDER);
+    //scale.set_offset(LOADCELL_OFFSET);
+    scale.tare();   //zero set
 
     // 4. Acquire reading
     Serial.print("Weight: ");
     M5.Lcd.clearDisplay(0);
-    Serial.println(load1.get_units(10), 2);
+    Serial.println(scale.get_units(5), 2);// print the average of 5 readings from the ADC minus tare weight, divided by the SCALE parameter set with set_scale
+
+/*
+if (scale.is_ready()) {
+    long reading = scale.read();
+    Serial.print("HX711 reading: ");
+*/
+
 
 }
 
 void dispLoad(void){
-    Serial.println(load1.get_units(10), 2);
+    Serial.println(scale.get_units(10), 2);
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,10 @@
 
 #define PIN_LOAD  36
 #define PIN_BAT_ON  19  //13 BASIC
-#define PIN_SERVO 26
+#define PIN_SERVO 33 // 32??   basic- 5, 2
+
+#define LOADCELL_DOUT_PIN 16////////////2
+#define LOADCELL_SCK_PIN  17////////////3
 
 
 uint8_t fmVer[] = "3.00";
@@ -27,12 +30,28 @@ Servo servo1;
 int16_t val;
 int8_t pos;
 
+
+
 void setup() {
   pinMode(PIN_LOAD, ANALOG);
   pinMode(PIN_BAT_ON, OUTPUT);
 
-  servo1.attach(PIN_SERVO);
-  servo1.write(pos);//pos 0-180///////////////////////
+  //ESP32servo
+  // Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+  servo1.setPeriodHertz(50);    // standard 50 hz servo
+  servo1.attach(PIN_SERVO, 1000, 2000); 
+  // using default min/max of 1000us and 2000us
+	// different servos may require different min/max settings
+	// for an accurate 0 to 180 sweep
+
+
+
+
+  servo1.write(pos);//pos 0-180///////////////////////サーボ角度0〜180
 
   Serial.begin(115200);
   digitalWrite(PIN_BAT_ON, LOW);    //サーボ用バッテリ電源オフ
@@ -54,7 +73,7 @@ void setup() {
   M5.Display.setFont(&fonts::lgfxJapanGothicP_16);
   M5.Display.setTextColor(TFT_WHITE, TFT_BLUE);
   M5.Display.setCursor(4, 0);
-  M5.Display.printf("電気タマボー ver.%s", (char*)fmVer);
+  M5.Display.printf("電気タマボーM5 ver.%s", (char*)fmVer);
 
 }
 
