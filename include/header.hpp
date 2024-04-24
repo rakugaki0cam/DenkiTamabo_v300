d04f65707eafcc269773622b47106be8b7fbcb7d
rakugaki0cam <isober21@gmail.com>
2024-04-24 22:59:47 +0900

    time wifi

--- include/header.hpp
+++ include/header.hpp
@@ -6,6 +6,10 @@
 
 #include <SD.h>
 #include <M5Unified.h>
+#include <WiFi.h>
+#include "time.h"
+#include "esp_sntp.h"
+
 //usr
 #include "servo.hpp"
 #include "scale.hpp"
@@ -18,6 +22,9 @@
 extern uint16_t measCnt;   //測定回数 
 extern uint8_t fmVer[];
 
+uint8_t wifiInit(void);
+void printLocalTime(void);
+void timeavailable(struct timeval *t);
 
 uint8_t sdInit(void);
 
